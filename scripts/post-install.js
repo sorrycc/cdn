5b34f554d04175b92efc306bb5817db7677ac165
afc163 <afc163@gmail.com>
2013-05-22 10:51:58 +0800

    console color

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -11,7 +11,7 @@ try {
 } catch(e) {
   console.log(' you need install spm to register the program');
   console.log();
-  console.log(' \x1b[31m$ npm install spm -g\x1b[39m');
+  console.log('$ npm install spm -g');
   console.log();
   console.log(" if you have installed spm, it maybe you haven't set a NODE_PATH environment variable");
   console.log();
